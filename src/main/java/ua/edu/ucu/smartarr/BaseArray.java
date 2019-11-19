def0b09183fb37a1a2e8cd1ba75fae581a5629a1
Yana Kryshchuk <kryshchuk@ucu.edu.ua>
2019-11-19 10:05:18 +0200

    Fixed style issues

--- src/main/java/ua/edu/ucu/smartarr/BaseArray.java
+++ src/main/java/ua/edu/ucu/smartarr/BaseArray.java
@@ -1,12 +1,16 @@
 package ua.edu.ucu.smartarr;
 
 
+import java.util.Arrays;
+
 // Base array for decorators
 public class BaseArray implements SmartArray {
     private Object[] array;
 
     public BaseArray(Object[] array) {
-        this.array = array;
+        if (array != null) {
+            this.array = Arrays.copyOf(array, array.length);
+        }
     }
 
     @Override
